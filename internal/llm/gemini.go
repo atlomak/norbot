76ab10393119fba71d71b4730c2a3755b4ea7932
atlomak <andrzej.tlomak01@gmail.com>
2025-01-13 02:32:36 +0100

    refactor: cheaper model for tests xD

--- internal/llm/gemini.go
+++ internal/llm/gemini.go
@@ -123,7 +123,7 @@ func sortActions(actions []Action) {
 
 func InitGeminiModel(client *genai.Client, ctx context.Context) *GeminiModel {
 
-	model := client.GenerativeModel("gemini-1.5-pro")
+	model := client.GenerativeModel("gemini-1.5-flash")
 	model.ResponseMIMEType = "application/json"
 	model.ResponseSchema = &genai.Schema{
 		Type: genai.TypeArray,
